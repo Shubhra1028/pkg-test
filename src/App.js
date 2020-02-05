14cfce533c863aaeb6f989bd7da2758aa88333ce
Shubhra1028 <shubhra1028@gmail.com>
2020-02-06 02:04:09 +0530

    init

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Shubhra <span role="img" aria-label="victory" >✌🏻</span>
         </a>
       </header>
     </div>
